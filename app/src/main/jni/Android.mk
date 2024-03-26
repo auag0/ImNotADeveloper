9f70b1a710517c56bc41d1f6eb5fdc49be22eef1
anago <outpe1119@gmail.com>
2024-03-27 05:07:58 +0900

    changed native hook method from Dobby to LSPosed

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -4,10 +4,4 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := ImNotADeveloper
 LOCAL_SRC_FILES := main.cpp
 LOCAL_LDLIBS := -llog
-LOCAL_STATIC_LIBRARIES := Dobby
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := Dobby
-LOCAL_SRC_FILES := Dobby/$(TARGET_ARCH_ABI)/libdobby.a
-include $(PREBUILT_STATIC_LIBRARY)
