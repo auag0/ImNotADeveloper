e24898801b940013ec1e8f42ffdf407684e12297
anago <outpe1119@gmail.com>
2024-03-27 05:09:24 +0900

    updated app version from 1.0.0 to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.auag0.imnotadeveloper"
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
